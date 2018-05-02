5609961fec24e2233303d9720c82ff5493927317
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-05-03 02:44:48 +0300

    Delete Map.jsx

--- src/Map.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Map = () =>
-  (<div><h1>Карта</h1><iframe title="Lomo" src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad01625454c5a0288661e4300300de0dd9d5d76d27ee993690e12bdc66ecf189b&amp;source=constructor" width="320" height="312" frameBorder="0"></iframe></div>);
-
-export default Map;
