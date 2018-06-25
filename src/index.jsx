5553cb0845498ae0214fadbc4be1909b8d069219
Anton Galkin <artemis9898@mail.ru>
2018-06-25 14:56:15 +0300

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import { render as r } from 'react-dom';
 
-const Stars = ({ length }) =>
-  <span>{Array.from({ length }, (v, i) => ++i).map(() => '⭐️').join('')}</span>;
+const data = {
+     "users":[
+     {
+      "login":"student",
+      "password":"tneduts"
+     },
+     {
+      "login":"myuserlogin",
+      "password":"mypas"
+     },
+     {
+      "login":"prof",
+      "password":"fessor"
+     },
+     {
+      "login":"egos",
+      "password":"selfish"
+     }]
+};
+
+function GetStars(n) {
+	let r = '';
+	for(let i = 0; i < n; i++) {
+		r = r + '⭐️';
+	}
+	return r;
+}
+
+const Person = ({stars, text}) => <span><b>{ text.toUpperCase() }</b>: { GetStars(stars) }</span>;
+// yarn run build
+
+const PersonList = () => {
+	return data.users.map((user, i) =><div>{i+1}: <Person text={user.login} stars={user.login.length} /></div>)
+}
+
+
 r(
-  <div><Stars length="5" /></div>,
-  document.querySelector('.cont'),
+    <PersonList />,
+	document.querySelector('.cont'),
 );
